260ce2d91ffbe0dce0eaea359d618d79a2913d4d
Roman Khomenko <romankhomenko1995@gmail.com>
2017-12-29 12:12:40 +0300

    class Vertex changed for using my matrix realization

--- include/Vertex.hpp
+++ include/Vertex.hpp
@@ -6,21 +6,31 @@
 #ifndef CG_LAB_VERTEX_HPP_
 #define CG_LAB_VERTEX_HPP_
 
-#include <QVector3D>
+#include <Matrix.hpp>
+
+using Matrix4x4 = Matrix<float, 4, 4>;
 
 class Vertex {
 public:
-    using PositionType = QVector3D;
+    using PositionType = Matrix<float, 1, 4>;
     using ReferencePositionType = PositionType&;
     using ConstReferencePositionType = const PositionType&;
 
     using FloatType = float;
     using IntType = std::int32_t;
 
-    constexpr Vertex() : Position{0.0, 0.0, 0.0} {}
-    constexpr Vertex(FloatType x, FloatType y) : Position{x, y, 0.0} {}
+    constexpr Vertex() : Vertex{0.0, 0.0, 0.0, 1.0} {}
+    constexpr Vertex(FloatType x, FloatType y) : Vertex{x, y, 0.0, 1.0} {}
+    constexpr Vertex(FloatType x, FloatType y, FloatType z)
+        : Vertex{x, y, z, 1.0} {}
+    constexpr Vertex(FloatType x, FloatType y, FloatType z, FloatType h)
+        : Position{x, y, z, h} {}
     constexpr Vertex(ConstReferencePositionType position)
         : Position{position} {}
+
+    constexpr Vertex operator*(const Matrix4x4& matrix) const {
+        return Position * matrix;
+    }
 
     constexpr ConstReferencePositionType GetPositon() const { return Position; }
 
